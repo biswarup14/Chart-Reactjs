7e09a7e50fb0402018e35f347e411a3c0aef97a8
github_biswarup14 <biswarup14rick@gmail.com>
2021-06-03 15:09:36 +0530

    Initail commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chart from './components/Chart'
+
+import './App.css';
+
+const App = ()=> {
+  return( 
+    <Chart />
+  )
+}
+
+export default App;
---
+++ src/components/Chart.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+
+const Chart = () => {
+  return (
+    <div>
+      <Line
+        data={{
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [
+            {
+              label: '# of votes',
+              data: [12, 19, 3, 5, 2, 3],
+              animations:{
+                  tension:{
+                    duration: 1000,
+                    easing: 'easeInExpo',
+                    from: 1,
+                    to: 0,
+                    loop: true
+                  }
+                },
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={500}
+        width={500}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              fontSize: 25,
+            },
+          },
+        }}
+      />
+    </div>
+  )
+}
+
+export default Chart
